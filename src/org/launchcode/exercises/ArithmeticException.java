aabb3fbf4f750bb2ca2a1904a63402fb20190581
arielmitchell <ariel.l.makowski@gmail.com>
2022-08-15 16:52:06 -0400

    Exception exercises

---
+++ src/org/launchcode/exercises/ArithmeticException.java
@@ -0,0 +1,7 @@
+package org.launchcode.exercises;
+
+public class ArithmeticException extends Exception {
+    public ArithmeticException(String message) {
+        super(message);
+    }
+}
--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -1,5 +1,7 @@
 package org.launchcode.exercises;
 
+import org.launchcode.Temperature.TemperatureException;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Test out your Divide() function!
+        //Divide (4, 0);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -15,17 +17,46 @@ public class Main {
         studentFiles.put("Elizabeth", "MyCode.java");
         studentFiles.put("Stefanie", "CoolProgram.java");
 
-        // Test out your CheckFileExtension() function!
+//        for (String file : studentFiles.values()) {
+//            CheckFileExtension(file);
+//        }
+
+        for (Map.Entry<String, String> student : studentFiles.entrySet()) {
+            int points = CheckFileExtension(student.getValue());
+            if (points == -1) {
+                System.out.println(student.getKey() + " did not submit a file.");
+            } else if (points == 0) {
+                System.out.println(student.getKey() + " did not submit the correct file type.");
+            }
+        }
     }
 
-    public static void Divide(int x, int y)
+    public static int Divide(int x, int y)
     {
-        // Write code here!
+        if (y == 0) {
+            try {
+                throw new ArithmeticException("You cannot divide by zero, it will create a black hole!");
+            } catch (ArithmeticException e){
+                e.printStackTrace();
+            }
+        }
+        return (x / y);
     }
 
     public static int CheckFileExtension(String fileName)
     {
-        return 0;
+        int points = 0;
+        if (fileName.contains(".java")) {
+            points++;
+        } else if (fileName == null || fileName == "") {
+            try {
+                throw new IllegalArgumentException("No file name specified!");
+            } catch (IllegalArgumentException e){
+                e.printStackTrace();
+            }
+            points--;
+        }
+        return points;
     }
 
 }
